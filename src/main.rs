756611b6b980169e44763c8847652e1e7cde5c7e
Andreas Rammhold <andreas@rammhold.de>
2017-11-19 14:32:43 +0100

    Implemented remaining `batctl` statistic fields

--- src/main.rs
+++ src/main.rs
@@ -24,94 +24,141 @@ struct DeviceStatistics {
     tx_dropped_packets_total: Gauge,
     rx_packets_total: Gauge,
     rx_bytes_total: Gauge,
-//    forward_packets_total: Gauge,
-//    forward_bytes_total: Gauge,
-//    mgmt_tx_total: Gauge,
-//    mgmt_tx_bytes_total: Gauge,
-//    mgmt_rx_counter: Gauge,
-//    mgmt_rx_bytes_total: Gauge,
-//    frag_tx_packets_total: Gauge,
-//    frag_tx_bytes_total: Gauge,
-//    frag_rx_packets_total: Gauge,
-//    frag_rx_bytes_total: Gauge,
-//    frag_fwd_packets_total: Gauge,
-//    tt_request_tx_packets_total: Gauge,
-//    tt_request_rx_packets_total: Gauge,
-//    tt_response_rx_packets_total: Gauge,
-//    tt_roam_adv_tx_packets_total: Gauge,
-//    tt_roam_adv_rx_packets_total: Gauge,
-//    dat_get_tx_packets_total: Gauge,
-//    dat_get_rx_packets_total: Gauge,
-//    dat_put_tx_packets_total: Gauge,
-//    dat_put_rx_packets_total: Gauge,
-//    dat_cached_reply_tx_packets_total: Gauge
+    forward_packets_total: Gauge,
+    forward_bytes_total: Gauge,
+    mgmt_tx_packets_total: Gauge,
+    mgmt_tx_bytes_total: Gauge,
+    mgmt_rx_packets_total: Gauge,
+    mgmt_rx_bytes_total: Gauge,
+    frag_tx_packets_total: Gauge,
+    frag_tx_bytes_total: Gauge,
+    frag_rx_packets_total: Gauge,
+    frag_rx_bytes_total: Gauge,
+    frag_fwd_packets_total: Gauge,
+
+    tt_request_tx_packets_total: Gauge,
+    tt_request_rx_packets_total: Gauge,
+    tt_response_rx_packets_total: Gauge,
+    tt_roam_adv_tx_packets_total: Gauge,
+    tt_roam_adv_rx_packets_total: Gauge,
+    dat_get_tx_packets_total: Gauge,
+    dat_get_rx_packets_total: Gauge,
+    dat_put_tx_packets_total: Gauge,
+    dat_put_rx_packets_total: Gauge,
+    dat_cached_reply_tx_packets_total: Gauge,
+}
+
+
+macro_rules! init_gauge {
+    ($field_name:ident, $labels:expr) => {
+        {
+        let name = stringify!($field_name);
+        let description = name.replace("_", " ");
+        register_gauge!(opts!(
+            name,
+            &description,
+            $labels
+        )).unwrap()
+        }
+    }
 }
 
 impl DeviceStatistics {
     fn new(device: &str) -> Result<DeviceStatistics, String> {
+        let labels = labels!{"device" => device,};
         let d = DeviceStatistics {
             device: String::from_str(device).unwrap(),
-            tx_packets_total: register_gauge!(opts!(
-                "tx_packets_total",
-                "tx packet gauge",
-                labels!{"device" => device,}
-            )).unwrap(),
-            tx_bytes_total: register_gauge!(opts!(
-                "tx_bytes_total",
-                "tx bytes gauge",
-                labels!{"device" => device,}
-            )).unwrap(),
-            tx_dropped_packets_total: register_gauge!(opts!(
-                "tx_dropped_packets_total",
-                "tx dropped packets gauge",
-                labels!{"device" => device,}
-            )).unwrap(),
-            rx_packets_total: register_gauge!(opts!(
-                "tx_packets_total",
-                "tx packets gauge",
-                labels!{"device" => device,}
-            )).unwrap(),
-            rx_bytes_total: register_gauge!(opts!(
-                "tx_bytes_total",
-                "tx bytes gauge",
-                labels!{"device" => device,}
-            )).unwrap(),
+            tx_packets_total: init_gauge!(tx_packets_total, labels),
+            tx_bytes_total: init_gauge!(tx_bytes_total, labels),
+            tx_dropped_packets_total: init_gauge!(tx_dropped_packets_total, labels),
+            rx_packets_total: init_gauge!(rx_packets_total, labels),
+            rx_bytes_total: init_gauge!(rx_bytes_octal, labels),
+            forward_packets_total: init_gauge!(forward_packets_total, labels),
+            forward_bytes_total: init_gauge!(forward_bytes_total, labels),
+            mgmt_tx_packets_total: init_gauge!(mgmt_tx_packets_total, labels),
+            mgmt_tx_bytes_total: init_gauge!(mgmt_tx_bytes_total, labels),
+            mgmt_rx_packets_total: init_gauge!(mgmt_rx_packets_total, labels),
+            mgmt_rx_bytes_total: init_gauge!(mgmt_rx_bytes_total, labels),
+            frag_tx_packets_total: init_gauge!(frag_tx_packets_total, labels),
+            frag_tx_bytes_total: init_gauge!(frag_tx_bytes_total, labels),
+            frag_rx_packets_total: init_gauge!(frag_rx_packets_total, labels),
+            frag_rx_bytes_total: init_gauge!(frag_rx_bytes_total, labels),
+            frag_fwd_packets_total: init_gauge!(frag_fwd_packets_total, labels),
+            tt_request_tx_packets_total: init_gauge!(tt_request_tx_packets_total, labels),
+            tt_request_rx_packets_total: init_gauge!(tt_request_rx_packets_total, labels),
+            tt_response_rx_packets_total: init_gauge!(tt_response_rx_packets_total, labels),
+            tt_roam_adv_tx_packets_total: init_gauge!(tt_roam_adv_tx_packets_total, labels),
+            tt_roam_adv_rx_packets_total: init_gauge!(tt_roam_adv_rx_packets_total, labels),
+            dat_get_tx_packets_total: init_gauge!(dat_get_tx_packets_total, labels),
+            dat_get_rx_packets_total: init_gauge!(dat_get_rx_packets_total, labels),
+            dat_put_tx_packets_total: init_gauge!(dat_put_tx_packets_total, labels),
+            dat_put_rx_packets_total: init_gauge!(dat_put_rx_packets_total, labels),
+            dat_cached_reply_tx_packets_total: init_gauge!(
+                dat_cached_reply_tx_packets_total,
+                labels
+            ),
         };
         Ok(d)
     }
 
-    fn update(&mut self) -> std::io::Result<()> {
-        let raw_statistics = get_batadv_statistics(&self.device)?;
-        let statistics = parse_batadv_statistics(&raw_statistics).unwrap();
+    fn update_batctl_statistics(&mut self) -> std::io::Result<()> {
+        let raw_statistics = get_batman_statistics(&self.device)?;
+        let statistics = parse_batman_statistics(&raw_statistics).unwrap();
         println!("statistics: {:?}", statistics);
 
         let zero: f64 = 0.0;
 
-        self.tx_packets_total.set(
-            *statistics.get("tx").unwrap_or(&zero),
-        );
-        self.tx_bytes_total.set(
-            *statistics.get("tx_bytes").unwrap_or(
-                &zero,
-            ),
-        );
-        self.tx_dropped_packets_total.set(*statistics
-            .get("tx_dropped")
-            .unwrap_or(&zero));
-        self.rx_packets_total.set(
-            *statistics.get("rx").unwrap_or(&zero),
-        );
-        self.rx_bytes_total.set(
-            *statistics.get("rx_bytes").unwrap_or(
-                &zero,
-            ),
-        );
+        let get = |key| match statistics.get(key) {
+            Some(v) => *v,
+            None => { println!("{} is undefined!", key); zero}
+        }; //_or(&zero);
+
+        self.tx_packets_total.set(get("tx"));
+        self.tx_bytes_total.set(get("tx_bytes"));
+        self.tx_dropped_packets_total.set(get("tx_dropped"));
+        self.rx_packets_total.set(get("rx"));
+        self.rx_bytes_total.set(get("tx_bytes"));
+        self.forward_packets_total.set(get("forward"));
+        self.forward_bytes_total.set(get("forward_bytes"));
+        self.mgmt_tx_packets_total.set(get("mgmt_tx"));
+        self.mgmt_tx_bytes_total.set(get("mgmt_tx_bytes"));
+        self.mgmt_rx_packets_total.set(get("mgmt_rx"));
+        self.mgmt_rx_bytes_total.set(get("mgmt_rx_bytes"));
+        self.frag_tx_packets_total.set(get("frag_tx"));
+        self.frag_tx_bytes_total.set(get("frag_tx_bytes"));
+        self.frag_rx_packets_total.set(get("frag_rx"));
+        self.frag_rx_bytes_total.set(get("frag_rx_bytes"));
+        self.frag_fwd_packets_total.set(get("frag_fwd"));
+        self.tt_request_tx_packets_total.set(get("tt_request_tx"));
+        self.tt_request_rx_packets_total.set(get("tt_request_rx"));
+        self.tt_response_rx_packets_total.set(get("tt_response_rx"));
+        self.tt_roam_adv_tx_packets_total.set(get("tt_roam_adv_tx"));
+        self.tt_roam_adv_rx_packets_total.set(get("tt_roam_adv_rx"));
+        self.dat_get_tx_packets_total.set(get("dat_get_tx"));
+        self.dat_get_rx_packets_total.set(get("dat_get_rx"));
+        self.dat_put_tx_packets_total.set(get("dat_put_tx"));
+        self.dat_put_rx_packets_total.set(get("dat_put_rx"));
+        self.dat_cached_reply_tx_packets_total.set(get(
+            "dat_cached_reply_tx",
+        ));
+
         Ok(())
+
+    }
+
+    fn update_sysfs_statistics() -> std::io::Result<()> {
+        // FIXME: implement
+        Ok(()
+    }
+
+    fn update(&mut self) -> std::io::Result<()> {
+        self.update_batctl_statistics()?;
+        self.update_sysfs_statistics()?;
     }
 }
 
 
-fn get_batadv_statistics(device: &str) -> std::io::Result<String> {
+fn get_batman_statistics(device: &str) -> std::io::Result<String> {
     let output = match Command::new("batctl").args(&["-m", device, "s"]).output() {
         Ok(o) => o,
         Err(e) => return Err(e),
@@ -120,7 +167,7 @@ fn get_batadv_statistics(device: &str) -> std::io::Result<String> {
     Ok(String::from_utf8(output.stdout).unwrap())
 }
 
-fn parse_batadv_statistics(stats: &str) -> Result<HashMap<String, f64>, String> {
+fn parse_batman_statistics(stats: &str) -> Result<HashMap<String, f64>, String> {
     let values: Vec<(&str, f64)> = stats
         .split("\n")
         .map(|l| l.trim())
